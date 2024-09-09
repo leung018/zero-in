e84fab9622883b2d57aee6c56351162150ff8a86
Leung Cheng <monewckl@hotmail.com.hk>
2024-09-09 22:42:45 +0800

    Refactor build setting

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,25 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/**',
+          dest: '.'
+        },
+        {
+          src: 'manifest.json',
+          dest: '.'
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -14,7 +29,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        popup: 'index.html',
+        index: 'index.html',
         background: 'src/background.ts'
       },
       output: {
