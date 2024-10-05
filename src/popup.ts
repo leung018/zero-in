0f2ceb6471af9c31978406019da519ed681108a2
Leung Cheng <monewckl@hotmail.com.hk>
2024-10-05 12:28:21 +0800

    Rename to PopupApp

--- src/popup.ts
+++ src/popup.ts
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './PopupApp.vue'
 
 const app = createApp(App)
 app.use(createBootstrap())
