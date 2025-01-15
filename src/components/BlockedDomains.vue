f3cd25bade910554b31a434416e850e59b7c9a70
Leung Cheng <monewckl@hotmail.com.hk>
2025-01-15 22:41:04 +0800

    Add button save added domain

--- src/components/BlockedDomains.vue
+++ src/components/BlockedDomains.vue
@@ -6,10 +6,18 @@ const props = defineProps<{
   siteRulesService: SiteRulesService
 }>()
 const blockedDomains = ref<string[]>([])
+const newDomainName = ref<string>('')
 
 onMounted(async () => {
   blockedDomains.value = (await props.siteRulesService.getSiteRules()).blockedDomains
 })
+
+async function addDomain() {
+  await props.siteRulesService.save({
+    blockedDomains: [...blockedDomains.value, newDomainName.value]
+  })
+  blockedDomains.value = (await props.siteRulesService.getSiteRules()).blockedDomains
+}
 </script>
 
 <template>
@@ -17,9 +25,16 @@ onMounted(async () => {
     <h1 class="mb-4">Blocked Domains</h1>
     <form class="mb-4">
       <div class="mb-3">
-        <input type="text" class="form-control" placeholder="Enter your domain" required />
+        <input
+          v-model="newDomainName"
+          type="text"
+          class="form-control"
+          data-test="blocked-domain-input"
+          placeholder="Enter your domain"
+          required
+        />
       </div>
-      <button class="btn btn-primary" type="submit">Add Domain</button>
+      <button class="btn btn-primary" data-test="add-button" @click="addDomain">Add Domain</button>
     </form>
     <ul class="list-group">
       <li
--- src/components/__tests__/BlockedDomains.spec.ts
+++ src/components/__tests__/BlockedDomains.spec.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest'
 
 import BlockedDomains from '../BlockedDomains.vue'
 import { InMemorySiteRulesService, type SiteRulesService } from '../../domain/site_rules_service'
-import { flushPromises, mount } from '@vue/test-utils'
+import { flushPromises, mount, VueWrapper } from '@vue/test-utils'
 
 describe('BlockedDomains', () => {
   it('should render blocked domains', async () => {
@@ -18,6 +18,31 @@ describe('BlockedDomains', () => {
     expect(blockedDomains[0].text()).toBe('example.com')
     expect(blockedDomains[1].text()).toBe('facebook.com')
   })
+
+  it('should able to add new blocked domain', async () => {
+    const { wrapper, siteRulesService } = mountBlockedDomains()
+
+    await addBlockedDomain(wrapper, 'example.com')
+
+    let blockedDomains = wrapper.findAll("[data-test='blocked-domains']")
+    expect(blockedDomains.length).toBe(1)
+    expect(blockedDomains[0].text()).toBe('example.com')
+
+    expect((await siteRulesService.getSiteRules()).blockedDomains).toEqual(['example.com'])
+
+    await addBlockedDomain(wrapper, 'facebook.com')
+
+    blockedDomains = wrapper.findAll("[data-test='blocked-domains']")
+    expect(blockedDomains.length).toBe(2)
+
+    expect(blockedDomains[0].text()).toBe('example.com')
+    expect(blockedDomains[1].text()).toBe('facebook.com')
+
+    expect((await siteRulesService.getSiteRules()).blockedDomains).toEqual([
+      'example.com',
+      'facebook.com'
+    ])
+  })
 })
 
 function mountBlockedDomains(siteRulesService: SiteRulesService = new InMemorySiteRulesService()) {
@@ -25,5 +50,13 @@ function mountBlockedDomains(siteRulesService: SiteRulesService = new InMemorySi
     props: { siteRulesService }
   })
 
-  return { wrapper }
+  return { wrapper, siteRulesService }
+}
+
+async function addBlockedDomain(wrapper: VueWrapper, domain: string) {
+  const inputElement = wrapper.find("[data-test='blocked-domain-input']")
+  await inputElement.setValue(domain)
+
+  const addButton = wrapper.find("[data-test='add-button']")
+  await addButton.trigger('click')
 }
