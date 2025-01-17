cf0400cfbbe38002df3410feb63a8ce1eb8a96a2
Leung Cheng <monewckl@hotmail.com.hk>
2025-01-17 17:00:25 +0800

    Introduce SiteRules class

--- src/domain/site_rules.ts
+++ src/domain/site_rules.ts
@@ -1,3 +1,31 @@
 export type SiteRules = {
   blockedDomains: string[]
 }
+
+export class SiteRules2 {
+  private _blockedDomains: ReadonlyArray<string>
+
+  constructor({ blockedDomains = [] }: { blockedDomains?: string[] } = {}) {
+    this._blockedDomains = deduplicated(blockedDomains.map((domain) => domain.trim()))
+  }
+
+  public get blockedDomains(): ReadonlyArray<string> {
+    return this._blockedDomains
+  }
+}
+
+function deduplicated(inputs: string[]): string[] {
+  const set: Set<string> = new Set()
+  const results: string[] = []
+
+  inputs.forEach((domain) => {
+    if (set.has(domain)) {
+      return
+    }
+
+    set.add(domain)
+    results.push(domain)
+  })
+
+  return results
+}
