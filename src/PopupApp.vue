65f0551d5a19ce3c0e7637cb2cbda9014765c05d
Leung Cheng <monewckl@hotmail.com.hk>
2025-01-18 19:08:23 +0800

    Update Popup dependencies

--- src/PopupApp.vue
+++ src/PopupApp.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
+import { WebsiteRedirectServiceImpl } from './chrome/redirect'
 import BlockedDomains from './components/BlockedDomains.vue'
 import { SiteRulesStorageServiceImpl } from './domain/site_rules_storage'
 const siteRulesStorageService = SiteRulesStorageServiceImpl.create()
+const websiteRedirectService = new WebsiteRedirectServiceImpl()
+
+declare const chrome: any // FIXME: See other comments related to declare const chrome: any
+const targetRedirectUrl = chrome.runtime.getURL('options.html')
 </script>
 
 <template>
   <main>
-    <BlockedDomains :siteRulesStorageService="siteRulesStorageService" />
+    <BlockedDomains
+      :siteRulesStorageService="siteRulesStorageService"
+      :websiteRedirectService="websiteRedirectService"
+      :targetRedirectUrl="targetRedirectUrl"
+    />
   </main>
 </template>
