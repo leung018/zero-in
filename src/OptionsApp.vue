0de64e62a1a21960bcfd81b83373f21e70b02910
Leung Cheng <monewckl@hotmail.com.hk>
2025-01-23 22:32:36 +0800

    Update option page props

--- src/OptionsApp.vue
+++ src/OptionsApp.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import WeeklySchedulesEditor from './components/WeeklySchedulesEditor.vue'
+import { WeeklyScheduleStorageServiceImpl } from './domain/schedules/storage'
+
+const weeklyScheduleStorageService = WeeklyScheduleStorageServiceImpl.create()
 </script>
 
 <template>
   <main>
-    <WeeklySchedulesEditor />
+    <WeeklySchedulesEditor :weeklyScheduleStorageService="weeklyScheduleStorageService" />
   </main>
 </template>
