1c66cdd251da9e28333e09533dcd7a23752c7cc2
Leung Cheng <monewckl@hotmail.com.hk>
2025-01-25 12:41:56 +0800

    Add interface comment

--- src/domain/browsing_rules/redirect.ts
+++ src/domain/browsing_rules/redirect.ts
@@ -1,5 +1,8 @@
 import type { BrowsingRules } from '.'
 
 export interface WebsiteRedirectService {
+  /**
+   * This method will activate the redirect to the target URL according to the browsing rules. Any old activated browsing rules will be discarded.
+   */
   activateRedirect(browsingRules: BrowsingRules, targetUrl: string): Promise<void>
 }
