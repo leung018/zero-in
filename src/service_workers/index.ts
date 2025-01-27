30a9a36c4f96e05d39ef3646f3af4794020d72e8
Leung Cheng <monewckl@hotmail.com.hk>
2025-01-27 22:02:17 +0800

    Add comment

--- src/service_workers/index.ts
+++ src/service_workers/index.ts
@@ -6,6 +6,8 @@ declare const chrome: any // FIXME: See other comments related to declare const 
 const browsingRulesStorageService = BrowsingRulesStorageServiceImpl.create()
 const websiteRedirectService = new ChromeRedirectService()
 
+// FIXME: Below is to handle the case when the extension is disabled and than enabled again. Without this, the rules for redirection are not activated.
+// However, I cannot find a way to test this in the e2e tests and I only use manual testing for this.
 browsingRulesStorageService.get().then((browsingRules) => {
   websiteRedirectService.activateRedirect(browsingRules, chrome.runtime.getURL('options.html'))
 })
