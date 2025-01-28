4a066fc92831da855aea6832b8556968ecafd36a
Leung Cheng <monewckl@hotmail.com.hk>
2025-01-28 19:01:33 +0800

    Change to assert specific error in schedules

--- src/domain/schedules/schedules.spec.ts
+++ src/domain/schedules/schedules.spec.ts
@@ -1,37 +1,42 @@
 import { describe, expect, it } from 'vitest'
-import { Weekday, WeeklySchedule } from '.'
+import { Weekday, WeeklySchedule, WeeklyScheduleInvalidInputError } from '.'
 import { Time } from './time'
+import { assertToThrowError } from '../../test_utils/check_error'
 
 describe('WeeklySchedules', () => {
   it('should reject empty weekday set', () => {
-    expect(
+    assertToThrowError(
       () =>
         new WeeklySchedule({
           weekdaySet: new Set(),
           startTime: new Time(0, 0),
           endTime: new Time(0, 0)
-        })
-    ).toThrow('weekdaySet must not be empty')
+        }),
+      new WeeklyScheduleInvalidInputError('weekdaySet must not be empty')
+    )
   })
 
   it('should reject if start time is not before end time', () => {
-    const errMsg = 'startTime must be before endTime'
-    expect(
+    const err = new WeeklyScheduleInvalidInputError('startTime must be before endTime')
+
+    assertToThrowError(
       () =>
         new WeeklySchedule({
           weekdaySet: new Set([Weekday.Mon]),
           startTime: new Time(10, 0),
           endTime: new Time(9, 0)
-        })
-    ).toThrow(errMsg)
-    expect(
+        }),
+      err
+    )
+    assertToThrowError(
       () =>
         new WeeklySchedule({
           weekdaySet: new Set([Weekday.Mon]),
           startTime: new Time(10, 0),
           endTime: new Time(10, 0)
-        })
-    ).toThrow(errMsg)
+        }),
+      err
+    )
   })
 
   it('should accept valid input', () => {
