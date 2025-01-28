676b7ab86403bd73b6c6b89025e325e63354510e
Leung Cheng <monewckl@hotmail.com.hk>
2025-01-28 16:41:26 +0800

    Move test files outside __tests__

--- src/domain/schedules/time.spec.ts
+++ src/domain/schedules/time.spec.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { formatTimeNumber, Time } from '../time'
+import { formatTimeNumber, Time } from './time'
 
 describe('Time', () => {
   it('should reject invalid time', () => {
