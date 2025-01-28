fee36cc11c397ddab57e064589491848d6d875cc
Leung Cheng <monewckl@hotmail.com.hk>
2025-01-28 20:12:23 +0800

    Configure unplugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import webExtension from 'vite-plugin-web-extension'
+import Icons from 'unplugin-icons/vite'
+import Components from 'unplugin-vue-components/vite'
+import IconsResolve from 'unplugin-icons/resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), webExtension()]
+  plugins: [
+    vue(),
+    webExtension(),
+    Components({
+      resolvers: [IconsResolve()],
+      dts: true
+    }),
+    Icons({
+      compiler: 'vue3',
+      autoInstall: true
+    })
+  ]
 })
