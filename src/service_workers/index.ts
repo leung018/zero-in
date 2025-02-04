da6c45000ffb3d3e49267397162194776e8985a0
Leung Cheng <monewckl@hotmail.com.hk>
2025-02-04 18:41:02 +0800

    Remove debug logging

--- src/service_workers/index.ts
+++ src/service_workers/index.ts
@@ -4,7 +4,6 @@ const redirectTogglingService = RedirectTogglingService.create()
 
 chrome.alarms.create('redirectRules', { periodInMinutes: 0.01 }) // TODO: Set it to 0.5 in production
 chrome.alarms.onAlarm.addListener((alarm) => {
-  console.log('Alarm triggered', alarm)
   if (alarm.name === 'redirectRules') {
     redirectTogglingService.run()
   }
