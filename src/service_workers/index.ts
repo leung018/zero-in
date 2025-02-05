e66a6d04fe0f5dc1dbee9fa9a66d68fe0143d637
Leung Cheng <monewckl@hotmail.com.hk>
2025-02-05 14:28:06 +0800

    Update the service worker

--- src/service_workers/index.ts
+++ src/service_workers/index.ts
@@ -2,9 +2,12 @@ import { RedirectTogglingService } from '../domain/redirect_toggling'
 
 const redirectTogglingService = RedirectTogglingService.create()
 
-chrome.alarms.create('redirectRules', { periodInMinutes: 0.01 }) // TODO: Set it to 0.5 in production
+// Noted that e2e tests are hard to cover all of the below properly. Better use a bit manual testing if needed.
+
 chrome.alarms.onAlarm.addListener((alarm) => {
+  console.debug('Alarm fired:', alarm)
   if (alarm.name === 'redirectRules') {
-    redirectTogglingService.run()
+    return redirectTogglingService.run()
   }
 })
+chrome.alarms.create('redirectRules', { periodInMinutes: 0.5, when: Date.now() })
