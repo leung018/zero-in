45389b0c508e31390f4e5312dafaebd38fc57e4c
Leung Cheng <monewckl@hotmail.com.hk>
2025-02-11 14:21:06 +0800

    Add navigation

--- src/OptionsApp.vue
+++ src/OptionsApp.vue
@@ -2,6 +2,38 @@
 import WeeklySchedulesPage from './pages/WeeklySchedulesPage/index.vue'
 import { WeeklyScheduleStorageService } from './domain/schedules/storage'
 import { MessengerFactory } from './chrome/messenger'
+import BlockedDomainsPage from './pages/BlockedDomainsPage.vue'
+import { computed, ref, type Component } from 'vue'
+import { BrowsingRulesStorageService } from './domain/browsing_rules/storage'
+
+const sender = MessengerFactory.createMessenger()
+
+const routes: { [key: string]: { component: Component; props: object } } = {
+  '/': {
+    component: WeeklySchedulesPage,
+    props: {
+      weeklyScheduleStorageService: WeeklyScheduleStorageService.create(),
+      sender
+    }
+  },
+  '/blocked-domains': {
+    component: BlockedDomainsPage,
+    props: {
+      browsingRulesStorageService: BrowsingRulesStorageService.create(),
+      sender
+    }
+  }
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'] || routes['/']
+})
 </script>
 
 <template>
@@ -10,16 +42,13 @@ import { MessengerFactory } from './chrome/messenger'
       <span class="navbar-brand ms-2">Options</span>
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="#">Schedules</a>
+          <a class="nav-link" href="#/">Schedules</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Blocked Domains</a>
+          <a class="nav-link" href="#/blocked-domains">Blocked Domains</a>
         </li>
       </ul>
     </nav>
-    <WeeklySchedulesPage
-      :weeklyScheduleStorageService="WeeklyScheduleStorageService.create()"
-      :sender="MessengerFactory.createMessenger()"
-    />
+    <component :is="currentView.component" v-bind="currentView.props" />
   </main>
 </template>
