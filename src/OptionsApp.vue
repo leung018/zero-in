06e6ef0c0a5479d04ff1495d1b9531348e913450
Leung Cheng <monewckl@hotmail.com.hk>
2025-02-11 14:02:14 +0800

    Add nav ui

--- src/OptionsApp.vue
+++ src/OptionsApp.vue
@@ -6,6 +6,17 @@ import { MessengerFactory } from './chrome/messenger'
 
 <template>
   <main>
+    <nav class="navbar navbar-expand navbar-light bg-light">
+      <span class="navbar-brand ms-2">Options</span>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Schedules</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blocked Domains</a>
+        </li>
+      </ul>
+    </nav>
     <WeeklySchedulesPage
       :weeklyScheduleStorageService="WeeklyScheduleStorageService.create()"
       :sender="MessengerFactory.createMessenger()"
