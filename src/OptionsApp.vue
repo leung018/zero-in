1f54357b3ac09657deac716db243b18369da378b
Leung Cheng <monewckl@hotmail.com.hk>
2025-02-11 13:23:50 +0800

    Revert "Experiment with createRouter"
    This reverts commit bd4589bcbd77dad663c91bcd391ded31c1be9ce5.

--- src/OptionsApp.vue
+++ src/OptionsApp.vue
@@ -6,21 +6,9 @@ import { MessengerFactory } from './chrome/messenger'
 
 <template>
   <main>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Options</a>
-        <div class="collapse navbar-collapse">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <RouterLink class="nav-link" to="/schedules">Schedules</RouterLink>
-            </li>
-            <li class="nav-item">
-              <RouterLink class="nav-link" to="/blocked-domains">Blocked Domains</RouterLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-    <RouterView />
+    <WeeklySchedulesPage
+      :weeklyScheduleStorageService="WeeklyScheduleStorageService.create()"
+      :sender="MessengerFactory.createMessenger()"
+    />
   </main>
 </template>
--- src/options.ts
+++ src/options.ts
@@ -1,46 +1,11 @@
 import { createBootstrap } from 'bootstrap-vue-next'
-import WeeklySchedulesPage from './pages/WeeklySchedulesPage/index.vue'
-import BlockedDomainsPage from './pages/BlockedDomainsPage.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
 import { createApp } from 'vue'
 import OptionsApp from './OptionsApp.vue'
-import { createRouter, createWebHashHistory } from 'vue-router'
-import { WeeklyScheduleStorageService } from './domain/schedules/storage'
-import { MessengerFactory } from './chrome/messenger'
-import { BrowsingRulesStorageService } from './domain/browsing_rules/storage'
-
-const routes = [
-  {
-    path: '/schedules',
-    component: WeeklySchedulesPage,
-    props: {
-      weeklyScheduleStorageService: WeeklyScheduleStorageService.create(),
-      sender: MessengerFactory.createMessenger()
-    }
-  },
-  {
-    path: '/',
-    redirect: '/schedules'
-  },
-  {
-    path: '/blocked-domains',
-    component: BlockedDomainsPage,
-    props: {
-      sender: MessengerFactory.createMessenger(),
-      browsingRulesStorageService: BrowsingRulesStorageService.create()
-    }
-  }
-]
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
 
 const app = createApp(OptionsApp)
-app.use(router)
 app.use(createBootstrap())
 app.mount('#app')
