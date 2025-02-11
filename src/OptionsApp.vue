bd4589bcbd77dad663c91bcd391ded31c1be9ce5
Leung Cheng <monewckl@hotmail.com.hk>
2025-02-11 13:23:30 +0800

    Experiment with createRouter

--- src/OptionsApp.vue
+++ src/OptionsApp.vue
@@ -6,9 +6,21 @@ import { MessengerFactory } from './chrome/messenger'
 
 <template>
   <main>
-    <WeeklySchedulesPage
-      :weeklyScheduleStorageService="WeeklyScheduleStorageService.create()"
-      :sender="MessengerFactory.createMessenger()"
-    />
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Options</a>
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/schedules">Schedules</RouterLink>
+            </li>
+            <li class="nav-item">
+              <RouterLink class="nav-link" to="/blocked-domains">Blocked Domains</RouterLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <RouterView />
   </main>
 </template>
