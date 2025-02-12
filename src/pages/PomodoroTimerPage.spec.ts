4ab6b6139a8a39fa7b41a6a0de0e9e973bc30a7d
Leung Cheng <monewckl@hotmail.com.hk>
2025-02-12 12:54:40 +0800

    Refactor test case

--- src/pages/PomodoroTimerPage.spec.ts
+++ src/pages/PomodoroTimerPage.spec.ts
@@ -4,18 +4,13 @@ import { expect, describe, it } from 'vitest'
 import { Duration } from '../domain/pomodoro/duration'
 
 describe('PomodoroTimerPage', () => {
-  it('should start the timer', () => {
+  it('should display the time representing focus duration before timer is started', () => {
     const { wrapper } = mountPomodoroTimerPage({
       focusDuration: new Duration({ minutes: 9 })
     })
 
     const timerDisplay = wrapper.find("[data-test='timer-display']")
     expect(timerDisplay.text()).toBe('09:00')
-
-    const startButton = wrapper.find("[data-test='start-button']")
-    startButton.trigger('click')
-
-    // TODO
   })
 })
 
