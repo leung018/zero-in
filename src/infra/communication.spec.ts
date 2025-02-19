f468b5d21696d24bc635530e8da6e5c0984dddd8
Leung Cheng <monewckl@hotmail.com.hk>
2025-02-19 22:48:12 +0800

    Add test of FakeCommunicationManager

---
+++ src/infra/communication.spec.ts
@@ -0,0 +1,31 @@
+import { describe, it, expect } from 'vitest'
+import { FakeCommunicationManager } from './communication'
+
+describe('FakeCommunicationManager', () => {
+  it('should clientConnect port can send and receive message to the backgroundPort in listener', () => {
+    const fakeCommunicationManager = new FakeCommunicationManager()
+
+    let message1: any, message2: any
+    fakeCommunicationManager.addClientConnectListener((backgroundPort) => {
+      backgroundPort.addListener((incomingMessage) => {
+        backgroundPort.send(incomingMessage + ' World')
+      })
+    })
+
+    const clientPort1 = fakeCommunicationManager.clientConnect()
+    clientPort1.addListener((incomingMessage) => {
+      message1 = incomingMessage
+    })
+    clientPort1.send('Hello')
+
+    const clientPort2 = fakeCommunicationManager.clientConnect()
+    clientPort2.addListener((incomingMessage) => {
+      message2 = incomingMessage
+    })
+    clientPort2.send('Hi')
+
+    // each clientPort has unique connection to the backgroundPort, so they won't receive each other message
+    expect(message1).toBe('Hello World')
+    expect(message2).toBe('Hi World')
+  })
+})
