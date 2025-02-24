efe99d46ed2e62d48dfd22052e1ce6c00979a37b
Leung Cheng <monewckl@hotmail.com.hk>
2025-02-24 22:25:31 +0800

    Configure port of ReminderPage

--- src/ReminderApp.vue
+++ src/ReminderApp.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
+import { ChromeCommunicationManager } from './chrome/communication'
 import ReminderPage from './pages/ReminderPage.vue'
 </script>
 
 <template>
-  <ReminderPage />
+  <ReminderPage :port="new ChromeCommunicationManager().clientConnect()" />
 </template>
