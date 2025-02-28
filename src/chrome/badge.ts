f5a51eb240517b48a7a9d2fd64e41ac91b77dd02
Leung Cheng <monewckl@hotmail.com.hk>
2025-02-28 13:59:38 +0800

    Add comment

--- src/chrome/badge.ts
+++ src/chrome/badge.ts
@@ -1,6 +1,8 @@
 import type { BadgeDisplayService, Badge } from '../infra/badge'
 
 export class ChromeBadgeDisplayService implements BadgeDisplayService {
+  // Hard to e2e test this class. Use a bit manual testing.
+
   displayBadge(badge: Badge) {
     chrome.action.setBadgeText({ text: badge.text })
     chrome.action.setBadgeBackgroundColor({ color: badge.color.backgroundColor })
