6b625acdb17be7d2fdf67371dde13edc8658b35b
Leung Cheng <monewckl@hotmail.com.hk>
2025-03-04 16:54:43 +0800

    Add BootstrapVueNextResolver config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import webExtension from 'vite-plugin-web-extension'
 import Icons from 'unplugin-icons/vite'
 import Components from 'unplugin-vue-components/vite'
 import IconsResolve from 'unplugin-icons/resolver'
+import { BootstrapVueNextResolver } from 'bootstrap-vue-next'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +14,7 @@ export default defineConfig({
       additionalInputs: ['reminder.html']
     }),
     Components({
-      resolvers: [IconsResolve()],
+      resolvers: [IconsResolve(), BootstrapVueNextResolver()],
       dts: true
     }),
     Icons({
