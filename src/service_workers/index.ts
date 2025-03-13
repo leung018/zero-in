4f18371d85292897f92b42bc037daacf6f12445c
Leung Cheng <monewckl@hotmail.com.hk>
2025-03-13 18:49:48 +0800

    Bug fix freezing after  reopen browser

--- src/service_workers/index.ts
+++ src/service_workers/index.ts
@@ -1,9 +1,10 @@
 import { BackgroundListener } from './listener'
 import { BrowsingControlTogglingService } from '../domain/browsing_control_toggling'
 
+// Noted that e2e tests are hard to cover all of the below related to chrome api properly. Better use a bit manual testing if needed.
+
 const redirectTogglingService = BrowsingControlTogglingService.create()
 
-// Noted that e2e tests are hard to cover below related to alarms properly. Better use a bit manual testing if needed.
 chrome.alarms.onAlarm.addListener((alarm) => {
   console.debug('Alarm fired:', alarm)
   redirectTogglingService.run()
@@ -11,3 +12,5 @@ chrome.alarms.onAlarm.addListener((alarm) => {
 chrome.alarms.create({ periodInMinutes: 0.5, when: Date.now() })
 
 BackgroundListener.create().start()
+
+chrome.runtime.onStartup.addListener(function () {}) // This is a hack to keep the above run immediately after the browser is closed and reopened
