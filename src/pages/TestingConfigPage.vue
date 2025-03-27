4b21e83e3baf2351cb3bd298f43bc472bf64cb6e
Leung Cheng <leung-cheng@outlook.com>
2025-03-28 01:09:27 +0800

    Render the timer config according to storage

--- src/pages/TestingConfigPage.vue
+++ src/pages/TestingConfigPage.vue
@@ -1,4 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import config from '../config'
+import { PomodoroTimerConfigStorageService } from '../domain/pomodoro/config/storage'
+import { defineProps, onBeforeMount, ref } from 'vue'
+
+const { timerConfigStorageService } = defineProps<{
+  timerConfigStorageService: PomodoroTimerConfigStorageService
+}>()
+
+const timerConfig = ref(config.getDefaultPomodoroTimerConfig())
+
+onBeforeMount(async () => {
+  timerConfig.value = await timerConfigStorageService.get()
+})
+</script>
 
 <template>
   <div class="container">
@@ -7,20 +21,45 @@
       this tab
     </h1>
     <form>
-      <b-form-group label="Focus Duration (seconds)" data-test="focus-duration">
-        <b-form-input type="number" min="1"></b-form-input>
+      <b-form-group label="Focus Duration (seconds)">
+        <b-form-input
+          type="number"
+          min="1"
+          data-test="focus-duration"
+          :value="timerConfig.focusDuration.remainingSeconds()"
+        ></b-form-input>
       </b-form-group>
-      <b-form-group label="Short Break Duration (seconds)" data-test="short-break-duration">
-        <b-form-input type="number" min="1"></b-form-input>
+      <b-form-group label="Short Break Duration (seconds)">
+        <b-form-input
+          type="number"
+          min="1"
+          data-test="short-break-duration"
+          :value="timerConfig.shortBreakDuration.remainingSeconds()"
+        ></b-form-input>
       </b-form-group>
-      <b-form-group label="Long Break Duration (seconds)" data-test="long-break-duration">
-        <b-form-input type="number" min="1"></b-form-input>
+      <b-form-group label="Long Break Duration (seconds)">
+        <b-form-input
+          type="number"
+          min="1"
+          data-test="long-break-duration"
+          :value="timerConfig.longBreakDuration.remainingSeconds()"
+        ></b-form-input>
       </b-form-group>
-      <b-form-group label="Number of Pomodori Per Cycle" data-test="num-of-pomodori-per-cycle">
-        <b-form-input type="number" min="1"></b-form-input>
+      <b-form-group label="Number of Pomodori Per Cycle">
+        <b-form-input
+          type="number"
+          min="1"
+          data-test="num-of-pomodori-per-cycle"
+          :value="timerConfig.numOfPomodoriPerCycle"
+        ></b-form-input>
       </b-form-group>
-      <b-form-group label="Record Housekeeping Days" data-test="record-housekeeping-days">
-        <b-form-input type="number" min="1"></b-form-input>
+      <b-form-group label="Pomodoro Record House Keep Days">
+        <b-form-input
+          type="number"
+          min="1"
+          data-test="pomodoro-record-house-keep-days"
+          :value="timerConfig.pomodoroRecordHouseKeepDays"
+        ></b-form-input>
       </b-form-group>
       <div class="mt-4">
         <b-button variant="danger" type="button" class="me-2" data-test="save-button"
