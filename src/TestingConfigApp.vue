7f336ef9e06fbcfbf9e1bbd655770c3765e18a8e
Leung Cheng <leung-cheng@outlook.com>
2025-03-28 14:31:02 +0800

    Fix type error

--- src/TestingConfigApp.vue
+++ src/TestingConfigApp.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import TestingConfigPage from './pages/TestingConfigPage.vue'
+import { PomodoroTimerConfigStorageService } from './domain/pomodoro/config/storage'
 </script>
 
 <template>
-  <TestingConfigPage />
+  <TestingConfigPage :timerConfigStorageService="PomodoroTimerConfigStorageService.create()" />
 </template>
