63f280a2d310489134451007fa906dada070d2f2
Leung Cheng <leung-cheng@outlook.com>
2025-04-09 21:14:53 +0800

    Add timeout to close

--- src/chrome/sound.ts
+++ src/chrome/sound.ts
@@ -4,10 +4,16 @@ export class SoundService implements ActionService {
   // Require manual testing
 
   trigger(): void {
-    chrome.offscreen.createDocument({
-      url: 'offscreen.html',
-      reasons: ['AUDIO_PLAYBACK'],
-      justification: 'Playing notification sound'
-    })
+    chrome.offscreen
+      .createDocument({
+        url: 'offscreen.html',
+        reasons: ['AUDIO_PLAYBACK'],
+        justification: 'Playing notification sound'
+      })
+      .then(() => {
+        setTimeout(() => {
+          chrome.offscreen.closeDocument()
+        }, 3000) // Remember let it longer than the sound duration
+      })
   }
 }
