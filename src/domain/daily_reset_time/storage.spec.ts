a99a7d290c0d46ffa1ff935deebfedc9e00c007e
Leung Cheng <leung-cheng@outlook.com>
2025-04-18 17:38:43 +0800

    Add test of DailyResetTimeStorageService

---
+++ src/domain/daily_reset_time/storage.spec.ts
@@ -0,0 +1,18 @@
+import { describe, expect, it } from 'vitest'
+import { DailyResetTimeStorageService } from './storage'
+import { Time } from '../time'
+
+describe('DailyResetTimeStorageService', () => {
+  it('should return 00:00am if no DailyResetTime is saved', async () => {
+    const dailyResetTimeStorageService = DailyResetTimeStorageService.createFake()
+    expect(await dailyResetTimeStorageService.get()).toStrictEqual(new Time(0, 0))
+  })
+
+  it('should save and get DailyResetTime', async () => {
+    const dailyResetTimeStorageService = DailyResetTimeStorageService.createFake()
+    const dailyResetTime = new Time(12, 59)
+
+    await dailyResetTimeStorageService.save(dailyResetTime)
+    expect(await dailyResetTimeStorageService.get()).toStrictEqual(dailyResetTime)
+  })
+})
