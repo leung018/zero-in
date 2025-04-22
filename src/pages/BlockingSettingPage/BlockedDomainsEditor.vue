3fb5a42275dec832dbf536febb7f813755e86a65
Leung Cheng <leung-cheng@outlook.com>
2025-04-22 17:32:48 +0800

    Move the header to root page

--- src/pages/BlockingSettingPage/BlockedDomainsEditor.vue
+++ src/pages/BlockingSettingPage/BlockedDomainsEditor.vue
@@ -43,7 +43,6 @@ async function updateBrowsingRules(browsingRules: BrowsingRules) {
 </script>
 
 <template>
-  <h2 class="mb-3 mt-3">Blocked Domains</h2>
   <form @submit.prevent>
     <div class="mb-3">
       <input
--- src/pages/BlockingSettingPage/TimerIntegrationSetting.vue
+++ src/pages/BlockingSettingPage/TimerIntegrationSetting.vue
@@ -5,7 +5,6 @@ const disableBlockingDuringBreaks = ref(true)
 </script>
 
 <template>
-  <h2 class="mb-3 mt-3">Timer Integration</h2>
   <BFormCheckbox v-model="disableBlockingDuringBreaks">
     Pause blocking during breaks
   </BFormCheckbox>
