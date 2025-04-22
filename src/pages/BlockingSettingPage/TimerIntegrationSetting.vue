97b1c8e30c5d1365d7b781956447e063b6e69045
Leung Cheng <leung-cheng@outlook.com>
2025-04-22 17:11:29 +0800

    Move to TimerIntegrationSetting

---
+++ src/pages/BlockingSettingPage/TimerIntegrationSetting.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const disableBlockingDuringBreaks = ref(true)
+</script>
+
+<template>
+  <h2 class="mb-3 mt-3">Timer Integration</h2>
+  <BFormCheckbox v-model="disableBlockingDuringBreaks">
+    Pause blocking during breaks
+  </BFormCheckbox>
+</template>
--- src/pages/BlockingSettingPage/index.vue
+++ src/pages/BlockingSettingPage/index.vue
@@ -5,13 +5,11 @@ import { WeeklyScheduleStorageService } from '@/domain/schedules/storage'
 import ContentTemplate from '../components/ContentTemplate.vue'
 import BlockedDomainsEditor from './BlockedDomainsEditor.vue'
 import WeeklySchedulesEditor from './WeeklySchedulesEditor/index.vue'
-import { ref } from 'vue'
+import TimerIntegrationSetting from './TimerIntegrationSetting.vue'
 
 const { port } = defineProps<{
   port: Port
 }>()
-
-const disableBlockingDuringBreaks = ref(true)
 </script>
 
 <template>
@@ -23,10 +21,7 @@ const disableBlockingDuringBreaks = ref(true)
       />
     </BCard>
     <BCard class="mb-4 shadow-sm">
-      <h2 class="mb-3 mt-3">Timer Integration</h2>
-      <BFormCheckbox v-model="disableBlockingDuringBreaks">
-        Pause blocking during breaks
-      </BFormCheckbox>
+      <TimerIntegrationSetting />
     </BCard>
     <BCard class="mb-4 shadow-sm">
       <WeeklySchedulesEditor
