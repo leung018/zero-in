3292fdb0b5a4e7d88e76e6ceeed7433867fa72aa
Leung Cheng <leung-cheng@outlook.com>
2025-04-25 21:04:54 +0800

    Simplify error instance in WeeklySchedules

--- src/domain/schedules/schedules.spec.ts
+++ src/domain/schedules/schedules.spec.ts
@@ -5,38 +5,36 @@ import { assertToThrowError } from '../../test_utils/check_error'
 
 describe('WeeklySchedules', () => {
   it('should reject empty weekday set', () => {
-    assertToThrowError(
+    expect(
       () =>
         new WeeklySchedule({
           weekdaySet: new Set(),
           startTime: new Time(0, 0),
           endTime: new Time(0, 0)
-        }),
-      new WeeklyScheduleInvalidInputError('weekdaySet must not be empty')
-    )
+        })
+    ).toThrow('weekdaySet must not be empty')
   })
 
   it('should reject if start time is not before end time', () => {
-    const err = new WeeklyScheduleInvalidInputError('startTime must be before endTime')
+    const err = 'startTime must be before endTime'
 
-    assertToThrowError(
+    expect(
       () =>
         new WeeklySchedule({
           weekdaySet: new Set([Weekday.MON]),
           startTime: new Time(10, 0),
           endTime: new Time(9, 0)
-        }),
-      err
-    )
-    assertToThrowError(
+        })
+    ).toThrow(err)
+
+    expect(
       () =>
         new WeeklySchedule({
           weekdaySet: new Set([Weekday.MON]),
           startTime: new Time(10, 0),
           endTime: new Time(10, 0)
-        }),
-      err
-    )
+        })
+    ).toThrow(err)
   })
 
   it('should accept valid input', () => {
