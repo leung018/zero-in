2f687d4f0eda30904190e99b38ca285a7e0f2f05
Leung Cheng <leung-cheng@outlook.com>
2025-04-25 22:13:12 +0800

    Fix format

--- src/utils/format.spec.ts
+++ src/utils/format.spec.ts
@@ -1,5 +1,5 @@
-import { describe, expect, it } from "vitest"
-import { formatNumber, getNumberWithOrdinal } from "./format"
+import { describe, expect, it } from 'vitest'
+import { formatNumber, getNumberWithOrdinal } from './format'
 
 describe('formatNumber', () => {
   it('should format number', () => {
