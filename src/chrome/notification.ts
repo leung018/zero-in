64ed4cd271b5f17be45b1d3ea01b07da8f069ac5
Leung Cheng <leung-cheng@outlook.com>
2025-04-28 19:00:21 +0800

    Remove ChromeDesktopNotificationService

--- src/chrome/notification.ts
+++ src/chrome/notification.ts
@@ -1,22 +1,6 @@
-import type { ActionService } from '../infra/action'
 import type { DesktopNotifier } from '../infra/desktop_notification'
 
 // Require manual testing
-export class ChromeDesktopNotificationService implements ActionService {
-  trigger(): void {
-    chrome.notifications.clear(NOTIFICATION_ID).then(() => {
-      chrome.notifications.create(NOTIFICATION_ID, {
-        type: 'basic',
-        iconUrl: chrome.runtime.getURL('icon.png'),
-        title: 'Task Concentrator',
-        message: "Time's up!"
-      })
-    })
-  }
-}
-
-const NOTIFICATION_ID = 'time-up'
-
 export class ChromeDesktopNotifier implements DesktopNotifier {
   triggerNotification(notificationId: string, buttons: { title: string }[]): void {
     chrome.notifications.clear(notificationId).then(() => {
