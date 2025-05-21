8129fb89b8c7388421f03ebdb3755df8f7cff571
Leung Cheng <leung-cheng@outlook.com>
2025-05-21 18:04:27 +0800

    yarn dlx @eslint/migrate-config .eslintrc.cjs

---
+++ eslint.config.cjs
@@ -0,0 +1,39 @@
+const {
+    defineConfig,
+} = require("eslint/config");
+
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = defineConfig([{
+    extends: compat.extends(
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "@vue/eslint-config-typescript",
+        "@vue/eslint-config-prettier/skip-formatting",
+    ),
+
+    languageOptions: {
+        ecmaVersion: "latest",
+        parserOptions: {},
+    },
+}, {
+    files: ["e2e/**/*.{test,spec}.{js,ts,jsx,tsx}"],
+    extends: compat.extends("plugin:playwright/recommended"),
+}, {
+    files: ["**/*.vue"],
+
+    rules: {
+        "vue/multi-word-component-names": 0,
+    },
+}]);
