dbcb5003d4e948c0062440446a2b65d1aa0b0717
Leung Cheng <leung-cheng@outlook.com>
2025-05-22 18:58:55 +0800

    Remove import of rushstack

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,39 +1,38 @@
-const {
-    defineConfig,
-} = require("eslint/config");
+const { defineConfig } = require('eslint/config')
 
-const js = require("@eslint/js");
+const js = require('@eslint/js')
 
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
+const { FlatCompat } = require('@eslint/eslintrc')
 
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-require("@rushstack/eslint-patch/modern-module-resolution");
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
 
-module.exports = defineConfig([{
+module.exports = defineConfig([
+  {
     extends: compat.extends(
-        "plugin:vue/vue3-essential",
-        "eslint:recommended",
-        "@vue/eslint-config-typescript",
-        "@vue/eslint-config-prettier/skip-formatting",
+      'plugin:vue/vue3-essential',
+      'eslint:recommended',
+      '@vue/eslint-config-typescript',
+      '@vue/eslint-config-prettier/skip-formatting'
     ),
 
     languageOptions: {
-        ecmaVersion: "latest",
-        parserOptions: {},
-    },
-}, {
-    files: ["e2e/**/*.{test,spec}.{js,ts,jsx,tsx}"],
-    extends: compat.extends("plugin:playwright/recommended"),
-}, {
-    files: ["**/*.vue"],
+      ecmaVersion: 'latest',
+      parserOptions: {}
+    }
+  },
+  {
+    files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
+    extends: compat.extends('plugin:playwright/recommended')
+  },
+  {
+    files: ['**/*.vue'],
 
     rules: {
-        "vue/multi-word-component-names": 0,
-    },
-}]);
+      'vue/multi-word-component-names': 0
+    }
+  }
+])
