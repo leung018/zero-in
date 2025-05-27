6671471c2611b0ae499e607290d5862b603ecbc0
Leung Cheng <leung-cheng@outlook.com>
2025-05-27 13:36:35 +0800

    Update the name

--- manifest.js
+++ manifest.js
@@ -4,7 +4,7 @@ import process from 'process'
 const manifestConfig = {
   manifest_version: 3,
   minimum_chrome_version: '114',
-  name: process.env.NAME || 'Task Concentrator',
+  name: 'Task Concentrator BETA' + process.env.NAME_SUFFIX || '',
   description:
     'Helps you stay focused and productive with a customizable timer and website blocker —— perfect for work or study.',
   version: process.env.VERSION || '0.1',
