b35553d88b9d23a53cfc477abefc8c98f728e556
Leung Cheng <leung-cheng@outlook.com>
2025-05-28 18:58:23 +0800

    Rename to integration

--- src/domain/blocking_timer_integration/storage.spec.ts
+++ src/domain/blocking_timer_integration/storage.spec.ts
@@ -6,19 +6,19 @@ import type { BlockingTimerIntegrationSchemas } from './serialize'
 import { BlockingTimerIntegrationStorageService } from './storage'
 
 describe('BlockingTimerIntegrationStorageService', () => {
-  it('should get default setting when no setting is saved', async () => {
+  it('should get default integration setting when no integration setting is saved', async () => {
     const service = BlockingTimerIntegrationStorageService.createFake()
     expect(await service.get()).toStrictEqual(config.getDefaultBlockingTimerIntegration())
   })
 
-  it('should save and get setting', async () => {
+  it('should save and get integration setting', async () => {
     const service = BlockingTimerIntegrationStorageService.createFake()
-    const setting: BlockingTimerIntegration = {
+    const integration: BlockingTimerIntegration = {
       shouldPauseBlockingDuringBreaks: false,
       shouldPauseBlockingWhenTimerIsNotRunning: true
     }
-    await service.save(setting)
-    expect(await service.get()).toStrictEqual(setting)
+    await service.save(integration)
+    expect(await service.get()).toStrictEqual(integration)
   })
 
   it('should migrate properly', async () => {
