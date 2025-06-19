8f090ba6837db4677e06f54fc65b32190d629b46
Leung Cheng <leung-cheng@outlook.com>
2025-06-19 13:47:42 +0800

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,33 @@
-import { fileURLToPath } from 'node:url'
-import { configDefaults, defineConfig, mergeConfig } from 'vitest/config'
-import viteConfig from './vite.config'
+import vue from '@vitejs/plugin-vue'
+import { BootstrapVueNextResolver } from 'bootstrap-vue-next'
+import path from 'path'
+import IconsResolve from 'unplugin-icons/resolver'
+import Icons from 'unplugin-icons/vite'
+import Components from 'unplugin-vue-components/vite'
+import { configDefaults, defineConfig } from 'vitest/config'
 
-export default mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-      coverage: {
-        include: ['src/**']
-      }
+export default defineConfig({
+  plugins: [
+    vue() as any,
+    Components({
+      resolvers: [IconsResolve(), BootstrapVueNextResolver()],
+      dts: true
+    }),
+    Icons({
+      compiler: 'vue3',
+      autoInstall: true
+    })
+  ],
+  test: {
+    environment: 'jsdom',
+    exclude: [...configDefaults.exclude, 'e2e/**'],
+    coverage: {
+      include: ['src/**']
     }
-  })
-)
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
