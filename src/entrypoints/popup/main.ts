88edf58f2be5b2ddff9f0982a96ac5ddd3325728
Leung Cheng <leung-cheng@outlook.com>
2025-07-01 22:29:04 +0800

    Move util to separate file

--- src/entrypoints/popup/main.ts
+++ src/entrypoints/popup/main.ts
@@ -1,22 +1,7 @@
+import { wakeUpServiceWorkerIfIdle } from '../../infra/browser/wake_service_worker'
 import { mountNewApp } from '../../mount'
 import PopupApp from './App.vue'
 
 wakeUpServiceWorkerIfIdle().then(() => {
   mountNewApp(PopupApp)
 })
-
-async function wakeUpServiceWorkerIfIdle() {
-  // This function addresses a bug where the timer popup freezes at 00:00 when the service worker becomes idle.
-  // The bug only occurs in actual extension popups and won't appear if popup.html is opened as a regular page.
-  // It sends a PING message to wake up the service worker before mounting the app.
-  //
-  // Testing notes:
-  // - Difficult to test with e2e because Playwright cannot open real extension popups.
-  //   (Playwright can only open popup.html as a regular page, not as an extension popup)
-  // - To manually test:
-  //   1. Navigate to chrome://serviceworker-internals/
-  //   2. Stop the service worker
-  //   3. Open the timer popup to verify the freeze bug is resolved
-
-  return browser.runtime.sendMessage({ type: 'PING' })
-}
