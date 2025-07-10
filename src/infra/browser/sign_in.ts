2d7abd962d0b287d0d7435e44e55226993bb8599
Leung Cheng <leung-cheng@outlook.com>
2025-07-11 00:37:32 +0800

    Fix the handling of offscreen element

--- src/infra/browser/sign_in.ts
+++ src/infra/browser/sign_in.ts
@@ -1,3 +1,35 @@
+import { PublicPath } from 'wxt/browser'
+
+const OFFSCREEN_DOCUMENT_PATH = '/offscreen-sign-in.html'
+
+let creating: Promise<void> | null = null // A global promise to avoid concurrency issues
+async function setupOffscreenDocument(path: PublicPath) {
+  // Check all windows controlled by the service worker to see if one
+  // of them is the offscreen document with the given path
+  const offscreenUrl = browser.runtime.getURL(path)
+  const existingContexts = await browser.runtime.getContexts({
+    contextTypes: [browser.runtime.ContextType.OFFSCREEN_DOCUMENT],
+    documentUrls: [offscreenUrl]
+  })
+
+  if (existingContexts.length > 0) {
+    return
+  }
+
+  // create offscreen document
+  if (creating) {
+    await creating
+  } else {
+    creating = browser.offscreen.createDocument({
+      url: path,
+      reasons: [browser.offscreen.Reason.DOM_SCRAPING],
+      justification: 'authentication'
+    })
+    await creating
+    creating = null
+  }
+}
+
 async function getAuth() {
   const auth = await browser.runtime.sendMessage({
     type: 'firebase-auth',
@@ -11,12 +43,7 @@ async function getAuth() {
 }
 
 export async function firebaseAuth() {
-  // await browser.offscreen.closeDocument()
-  await browser.offscreen.createDocument({
-    url: 'offscreen-sign-in.html',
-    reasons: [browser.offscreen.Reason.DOM_SCRAPING],
-    justification: 'authentication'
-  })
+  await setupOffscreenDocument(OFFSCREEN_DOCUMENT_PATH)
 
   const auth = await getAuth()
     .then((auth) => {
