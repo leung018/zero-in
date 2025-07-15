aa40b5a4de82ea656938945511a39d99d8ca9fbc
Leung Cheng <leung-cheng@outlook.com>
2025-07-15 13:51:22 +0800

    Fix sign in url

--- src/config.ts
+++ src/config.ts
@@ -56,7 +56,7 @@ const getBadgeColorConfig = (): {
 }
 
 const getSignInUrl = () => {
-  return import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || 'https://zero-in-8211f.web.app'
+  return 'https://' + (import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || 'zero-in-8211f.web.app')
 }
 
 export default {
