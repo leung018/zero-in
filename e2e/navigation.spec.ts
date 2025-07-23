02d2963358843266a7a2d64213063f46b632e7de
Leung Cheng <leung-cheng@outlook.com>
2025-07-23 18:26:22 +0800

    Add test case

--- e2e/navigation.spec.ts
+++ e2e/navigation.spec.ts
@@ -2,7 +2,8 @@
 import { Page } from '@playwright/test'
 import { expect, test } from './fixtures.js'
 import { assertWithRetry } from './utils/assertion.js'
-import { goToFocusTimer } from './utils/navigation.js'
+import { goToBlockingSettingPage, goToFocusTimer } from './utils/navigation.js'
+import { enableSignInFeatureFlag } from './utils/operation.js'
 
 test('should clicking the options button in timer can go to options page', async ({
   page,
@@ -15,10 +16,36 @@ test('should clicking the options button in timer can go to options page', async
   await assertOpenedOptionsPage(page)
 })
 
+test('should clicking the sign in button go to sign in page', async ({ page, extensionId }) => {
+  await goToFocusTimer(page, extensionId)
+
+  await enableSignInFeatureFlag(page)
+  await page.reload()
+
+  await page.getByTestId('sign-in-button').click()
+
+  await assertOpenedSignInPage(page)
+
+  await goToBlockingSettingPage(page, extensionId)
+
+  await page.getByTestId('sign-in-button').click()
+  expect(await page.url().includes('sign-in.html')).toBeTruthy()
+
+  // The functionality inside sign In page need to test manually
+})
+
 export async function assertOpenedOptionsPage(page: Page) {
   return assertWithRetry(async () => {
     const pages = page.context().pages()
     const optionsPage = pages.find((p) => p.url().includes('options.html'))
     expect(optionsPage).toBeDefined()
+  })
+}
+
+export async function assertOpenedSignInPage(page: Page) {
+  return assertWithRetry(async () => {
+    const pages = page.context().pages()
+    const signInPage = pages.find((p) => p.url().includes('sign-in.html'))
+    expect(signInPage).toBeDefined()
   })
 }
