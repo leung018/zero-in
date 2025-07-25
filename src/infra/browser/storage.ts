accbef90e757a4c75190be0f7839259d48bfc3fa
Leung Cheng <leung-cheng@outlook.com>
2025-07-25 18:15:02 +0800

    Remove not using function

--- src/infra/browser/storage.ts
+++
@@ -1,7 +0,0 @@
-import { type LocalStorage } from '../storage/local_storage'
-
-export class BrowserStorageProvider {
-  static getLocalStorage(): LocalStorage {
-    return browser.storage.local
-  }
-}
