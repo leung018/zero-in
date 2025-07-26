b9c47afcbc1135be4df20cbe66300c8bdb9fc232
Leung Cheng <leung-cheng@outlook.com>
2025-07-26 14:33:47 +0800

    Do not share migration test

--- src/domain/schedules/storage.spec.ts
+++ src/domain/schedules/storage.spec.ts
@@ -1,5 +1,9 @@
-import { beforeEach, describe } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { Weekday, WeeklySchedule } from '.'
 import { LocalStorageWrapper } from '../../infra/storage/local_storage_wrapper'
+import { Time } from '../time'
+import { WeeklyScheduleSchemas } from './schema'
+import { WeeklyScheduleStorageService } from './storage'
 import { runWeeklyScheduleStorageServiceTests } from './storage_test'
 
 describe('WeeklyScheduleStorageService', () => {
@@ -10,4 +14,27 @@ describe('WeeklyScheduleStorageService', () => {
   })
 
   runWeeklyScheduleStorageServiceTests(storage)
+
+  it('should migrate properly', async () => {
+    const data: WeeklyScheduleSchemas[0] = [
+      {
+        weekdays: [0, 1],
+        startTime: { hour: 10, minute: 59 },
+        endTime: { hour: 12, minute: 12 }
+      }
+    ]
+    storage.set(WeeklyScheduleStorageService.STORAGE_KEY, data)
+
+    const service = new WeeklyScheduleStorageService(storage)
+    const expected: WeeklySchedule[] = [
+      new WeeklySchedule({
+        weekdaySet: new Set([Weekday.SUN, Weekday.MON]),
+        startTime: new Time(10, 59),
+        endTime: new Time(12, 12),
+        targetFocusSessions: undefined
+      })
+    ]
+    const result = await service.getAll()
+    expect(result).toStrictEqual(expected)
+  })
 })
--- src/domain/schedules/storage_test.ts
+++ src/domain/schedules/storage_test.ts
@@ -2,7 +2,6 @@ import { expect, it } from 'vitest'
 import { Weekday, WeeklySchedule } from '.'
 import { StorageInterface } from '../../infra/storage/interface'
 import { Time } from '../time'
-import { WeeklyScheduleSchemas } from './schema'
 import { WeeklyScheduleStorageService } from './storage'
 
 export function runWeeklyScheduleStorageServiceTests(storage: StorageInterface) {
@@ -29,28 +28,5 @@ export function runWeeklyScheduleStorageServiceTests(storage: StorageInterface) 
 
     await service.saveAll(weeklySchedules)
     expect(await service.getAll()).toStrictEqual(weeklySchedules)
-  })
-
-  it('should migrate properly', async () => {
-    const data: WeeklyScheduleSchemas[0] = [
-      {
-        weekdays: [0, 1],
-        startTime: { hour: 10, minute: 59 },
-        endTime: { hour: 12, minute: 12 }
-      }
-    ]
-    storage.set(WeeklyScheduleStorageService.STORAGE_KEY, data)
-
-    const service = new WeeklyScheduleStorageService(storage)
-    const expected: WeeklySchedule[] = [
-      new WeeklySchedule({
-        weekdaySet: new Set([Weekday.SUN, Weekday.MON]),
-        startTime: new Time(10, 59),
-        endTime: new Time(12, 12),
-        targetFocusSessions: undefined
-      })
-    ]
-    const result = await service.getAll()
-    expect(result).toStrictEqual(expected)
   })
 }
