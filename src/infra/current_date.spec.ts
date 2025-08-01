76e93586aa90b26f8d6783599058c202262ecc06
Leung Cheng <leung-cheng@outlook.com>
2025-08-01 18:39:42 +0800

    FakeTimeCounter can affect CurrentDateService

--- src/infra/current_date.spec.ts
+++ src/infra/current_date.spec.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from 'vitest'
+import { FakeTimeCounter } from '../utils/fake_time_counter'
 import { CurrentDateService } from './current_date'
 
 describe('CurrentDateService', () => {
@@ -10,5 +11,17 @@ describe('CurrentDateService', () => {
     const date2 = currentDateService.getDate()
 
     expect(date1.getTime()).toBeLessThan(date2.getTime())
+  })
+
+  it('should fakeTimeCounter can affect the getDate correctly', async () => {
+    const fakeTimeCounter = new FakeTimeCounter()
+    const currentDateService = CurrentDateService.createFake(
+      new Date('2023-01-01T00:00:00Z'),
+      fakeTimeCounter
+    )
+
+    fakeTimeCounter.advanceTime(1500)
+
+    expect(currentDateService.getDate()).toEqual(new Date('2023-01-01T00:00:01.500Z'))
   })
 })
--- src/infra/current_date.ts
+++ src/infra/current_date.ts
@@ -1,10 +1,18 @@
+import { FakeTimeCounter } from '../utils/fake_time_counter'
+
 export class CurrentDateService {
   static create() {
     return new CurrentDateService(() => new Date())
   }
 
-  static createFake(stubbedDate: Date = new Date()) {
-    return new CurrentDateService(() => stubbedDate)
+  static createFake(
+    stubbedDate: Date = new Date(),
+    fakeTimeCounter: FakeTimeCounter = new FakeTimeCounter()
+  ) {
+    const getNewDate: () => Date = () => {
+      return new Date(stubbedDate.getTime() + fakeTimeCounter.getElapsedSystemTime())
+    }
+    return new CurrentDateService(getNewDate)
   }
 
   getDate: () => Date
