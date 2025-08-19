bc430c37a03982399cb803432aaa2a8caa02c58d
Leung Cheng <leung-cheng@outlook.com>
2025-08-19 17:23:47 +0800

    Remove non using response

--- src/service_workers/response.ts
+++ src/service_workers/response.ts
@@ -3,7 +3,6 @@ import type { TimerStage } from '../domain/timer/stage'
 export enum WorkResponseName {
   TIMER_STATE,
   FOCUS_SESSION_RECORDS_UPDATED,
-  AUTH_SUCCESS,
   RESET_TIMER_CONFIG_SUCCESS
 }
 
@@ -20,10 +19,6 @@ export type WorkResponse =
     }
   | {
       name: WorkResponseName.FOCUS_SESSION_RECORDS_UPDATED
-    }
-  | {
-      name: WorkResponseName.AUTH_SUCCESS
-      payload: any // TODO: Replace with actual auth type if available
     }
   | {
       name: WorkResponseName.RESET_TIMER_CONFIG_SUCCESS
