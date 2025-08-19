079a212f75c30d96b4cb0a3fc1a353f038975c8e
Leung Cheng <leung-cheng@outlook.com>
2025-08-19 17:30:58 +0800

    Add note

--- src/infra/storage/interface.ts
+++ src/infra/storage/interface.ts
@@ -4,6 +4,8 @@ export interface StorageInterface {
 }
 
 export interface ObservableStorage extends StorageInterface {
+  // Note: Some storage implementations may not support change notifications,
+  // in which case onChange will have no effect when called.
   onChange: (key: string, callback: (data: any) => void) => Promise<Unsubscribe>
 }
 
