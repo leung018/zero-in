6c9134a5c3616f49e33b78a91883986c90355675
Leung Cheng <leung-cheng@outlook.com>
2025-08-28 20:45:26 +0800

    Update featureFlagsService

--- src/infra/feature_flags.spec.ts
+++ src/infra/feature_flags.spec.ts
@@ -1,34 +1,48 @@
 import { describe, expect, it } from 'vitest'
-import { FeatureFlagsService } from './feature_flags'
+import { FeatureFlag, FeatureFlagsService } from './feature_flags'
 
 describe('FeatureFlagsService', () => {
   it('should isEnabled return false if no feature flag has been enabled', async () => {
     const service = FeatureFlagsService.createFake()
-    const isEnabled = await service.isEnabled('non_existing_flag')
+    const isEnabled = await service.isEnabled('sign-in')
     expect(isEnabled).toBe(false)
   })
 
   it('should isEnabled return true for enabled feature flag only', async () => {
     const service = FeatureFlagsService.createFake()
-    await service.enable('enabled_flag')
+    await service.enable('sign-in')
 
-    let isEnabled = await service.isEnabled('enabled_flag')
-    expect(isEnabled).toBe(true)
+    expect(await service.isEnabled('sign-in')).toBe(true)
+    expect(await service.isEnabled(FeatureFlag.SIGN_IN)).toBe(true)
 
-    isEnabled = await service.isEnabled('non_enabled_flag')
-    expect(isEnabled).toBe(false)
+    expect(await service.isEnabled('place-holder')).toBe(false)
   })
 
   it('should able to disable the enabled flag', async () => {
     const service = FeatureFlagsService.createFake()
-    await service.enable('flag1')
-    await service.enable('flag2')
+    await service.enable('place-holder')
+    await service.enable('sign-in')
 
-    await service.disable('flag1')
-    let isEnabled = await service.isEnabled('flag1')
-    expect(isEnabled).toBe(false)
+    await service.disable('place-holder')
+    expect(await service.isEnabled('place-holder')).toBe(false)
 
-    isEnabled = await service.isEnabled('flag2')
-    expect(isEnabled).toBe(true)
+    expect(await service.isEnabled('sign-in')).toBe(true)
+  })
+
+  it('should enableAll', async () => {
+    const service = FeatureFlagsService.createFake()
+    await service.enableAll()
+
+    expect(await service.isEnabled('sign-in')).toBe(true)
+    expect(await service.isEnabled('place-holder')).toBe(true)
+  })
+
+  it('should disableAll', async () => {
+    const service = FeatureFlagsService.createFake()
+    await service.enableAll()
+    await service.disableAll()
+
+    expect(await service.isEnabled('sign-in')).toBe(false)
+    expect(await service.isEnabled('place-holder')).toBe(false)
   })
 })
