7a863afde1d5c6532a8142151c800d0b539f88bd
Leung Cheng <leung-cheng@outlook.com>
2025-08-28 20:01:17 +0800

    Fix data test id

--- e2e/utils/interactions/testing_config.ts
+++ e2e/utils/interactions/testing_config.ts
@@ -3,5 +3,5 @@ import { Page } from '@playwright/test'
 export async function changeFocusDuration(page: Page, seconds: number) {
   const input = page.getByTestId('focus-duration')
   await input.fill(seconds.toString())
-  await page.getByTestId('save-button').click()
+  await page.getByTestId('testing-config-save-button').click()
 }
