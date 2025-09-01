c344d6393a88fd7f8cd89f638692ecb4c59315b0
Leung Cheng <leung-cheng@outlook.com>
2025-09-01 21:44:21 +0800

    Add comment

--- src/entrypoints/testing-config/main.ts
+++ src/entrypoints/testing-config/main.ts
@@ -2,6 +2,7 @@ import { wakeUpServiceWorkerIfIdle } from '../../infra/browser/wake_service_work
 import { mountNewApp } from '../../mount'
 import TestingConfigApp from './App.vue'
 
+// If don't add wakeUpServiceWorkerIfIdle, e2e test will fail in my local environment sometimes when resetting the timer config.
 wakeUpServiceWorkerIfIdle().then(() => {
   mountNewApp(TestingConfigApp)
 })
