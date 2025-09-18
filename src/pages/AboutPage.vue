d2666752d1923d97d9cb10d0e7d6030184dc462b
Leung Cheng <leung-cheng@outlook.com>
2025-09-18 16:24:28 +0800

    Add about page

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ContentTemplate from '@/pages/components/ContentTemplate.vue'
+</script>
+
+<template>
+  <ContentTemplate title="About">
+    <p>This is a description of the about page.</p>
+  </ContentTemplate>
+</template>
