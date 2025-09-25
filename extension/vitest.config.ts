b7df96a8a9602f30a9d92bc8f7c0e1d731e4d8e3
Leung Cheng <leung-cheng@outlook.com>
2025-09-25 20:18:52 +0800

    Fix test

--- extension/vitest.config.ts
+++ extension/vitest.config.ts
@@ -49,7 +49,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src'),
+      '@shared': path.resolve(__dirname, '../shared/src')
     }
   }
 })
