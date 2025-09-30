faafead7c23e6551809b7e2a4764db2b30ebb58d
Leung Cheng <leung-cheng@outlook.com>
2025-10-01 01:43:51 +0800

    Make shared module work roughly

--- apps/extension/src/service_workers/index.ts
+++ apps/extension/src/service_workers/index.ts
@@ -1,3 +1,4 @@
+import { DEMO_CONST } from '@zero-in/shared/a'
 import { BrowserNewTabService } from '../infra/browser/new_tab'
 import { firebaseAuth } from '../infra/browser/sign_in'
 import { FirebaseServices } from '../infra/firebase/services'
@@ -6,6 +7,8 @@ import { BackgroundListener } from './listener'
 import { MenuItemId } from './menu_item_id'
 
 export default function main() {
+  console.log('Service worker started with DEMO const', DEMO_CONST)
+
   browser.runtime.onStartup.addListener(function () {}) // Register an empty onStartup listener to ensure the service worker activates immediately after browser restart
 
   const listener = BackgroundListener.create()
