a242e6d6d9fa3ca5c7ba899c1b1da5ca9167aea2
Leung Cheng <leung-cheng@outlook.com>
2025-10-15 16:49:02 +0800

    Draft app picker

--- apps/mobile/app/(tabs)/index.tsx
+++ apps/mobile/app/(tabs)/index.tsx
@@ -1,29 +1,16 @@
-import AppBlockerModule from '@/modules/app-blocker'
-import { StyleSheet, Text, View } from 'react-native'
+import { AppPickerView } from '@/modules/app-blocker'
+import { StyleSheet, View } from 'react-native'
 
 export default function HomeScreen() {
   return (
-    <View style={styles.titleContainer}>
-      <Text>{AppBlockerModule.hello()}</Text>
+    <View style={styles.container}>
+      <AppPickerView style={{ flex: 1 }} />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute'
+  container: {
+    flex: 1
   }
 })
