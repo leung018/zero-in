47e68f8282f91be63d9e28bbeb943963714f8550
Leung Cheng <leung-cheng@outlook.com>
2025-10-21 00:15:38 +0800

    Remove export in types

--- apps/mobile/modules/app-blocker/index.ts
+++ apps/mobile/modules/app-blocker/index.ts
@@ -1,5 +1,4 @@
 // Reexport the native module. On web, it will be resolved to AppBlockerModule.web.ts
 // and on native platforms to AppBlockerModule.ts
-export * from './src/AppBlocker.types'
 export { default } from './src/AppBlockerModule'
 export { default as AppPickerView } from './src/AppPickerView'
