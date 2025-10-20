3de6e3899a9ff45ad3cc0afa46278867d6470689
Leung Cheng <leung-cheng@outlook.com>
2025-10-20 21:11:29 +0800

    Rename function

--- apps/mobile/modules/app-blocker/src/AppPickerView.tsx
+++ apps/mobile/modules/app-blocker/src/AppPickerView.tsx
@@ -1,12 +1,12 @@
 import { requireNativeViewManager } from 'expo-modules-core'
 import { StyleProp, ViewStyle } from 'react-native'
 
-type AppBlockerViewProps = {
+type AppPickerViewProps = {
   style?: StyleProp<ViewStyle>
 }
 
 const NativeAppPickerView = requireNativeViewManager('AppBlocker')
 
-export default function AppPickerView({ style }: AppBlockerViewProps) {
+export default function AppPickerView({ style }: AppPickerViewProps) {
   return <NativeAppPickerView style={style} />
 }
