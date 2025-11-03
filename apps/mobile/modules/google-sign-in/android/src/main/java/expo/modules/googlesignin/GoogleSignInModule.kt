67db3b4a84a89430dcffd57593d0748a1a8be026
Leung Cheng <leung-cheng@outlook.com>
2025-11-04 00:03:45 +0800

    Add draft

--- apps/mobile/modules/google-sign-in/android/src/main/java/expo/modules/googlesignin/GoogleSignInModule.kt
+++ apps/mobile/modules/google-sign-in/android/src/main/java/expo/modules/googlesignin/GoogleSignInModule.kt
@@ -1,10 +1,19 @@
 package expo.modules.googlesignin
 
+import android.credentials.GetCredentialException
+import androidx.credentials.CredentialManager
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.GetCredentialResponse
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 import expo.modules.kotlin.records.Record
 import expo.modules.kotlin.records.Field
 import expo.modules.kotlin.Promise
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ConfigureOptions : Record {
     @Field
@@ -12,16 +21,46 @@ class ConfigureOptions : Record {
 }
 
 class GoogleSignInModule : Module() {
+    private val googleIdOptionBuilder = GetGoogleIdOption.Builder()
+    private lateinit var credentialManager: CredentialManager
 
-  override fun definition() = ModuleDefinition {
-    Name("GoogleSignIn")
+    override fun definition() = ModuleDefinition {
+        Name("GoogleSignIn")
 
-    Function("configure") { options: ConfigureOptions ->
+        OnCreate {
+            credentialManager = CredentialManager.create(appContext.reactContext!!)
+        }
 
+        Function("configure") { options: ConfigureOptions ->
+            googleIdOptionBuilder.setServerClientId(options.webClientId)
+        }
+
+        AsyncFunction("signIn") { promise: Promise ->
+            val googleIdOption: GetGoogleIdOption = googleIdOptionBuilder
+                .setAutoSelectEnabled(true)
+                .setAutoSelectEnabled(true)
+                .build()
+
+            val request: GetCredentialRequest = GetCredentialRequest.Builder()
+                .addCredentialOption(googleIdOption)
+                .build()
+
+            CoroutineScope(Dispatchers.Main).launch {
+                val result = credentialManager.getCredential(
+                    request = request,
+                    context = appContext.currentActivity!!
+                )
+                handleSignIn(result, promise)
+            }
+        }
     }
 
-    AsyncFunction("signIn") { promise: Promise ->
-       promise.resolve("")
+    private fun handleSignIn(result: GetCredentialResponse, promise: Promise) {
+        when (val credential = result.credential) {
+            is GoogleIdTokenCredential -> {
+                val idToken = credential.idToken
+                promise.resolve(idToken)
+            }
+        }
     }
-  }
 }
