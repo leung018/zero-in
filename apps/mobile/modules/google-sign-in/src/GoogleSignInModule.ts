34c71c056e98a68059781cfc2328ad1c6256843d
Leung Cheng <leung-cheng@outlook.com>
2025-11-03 23:26:59 +0800

    Adjust the module interface

--- apps/mobile/modules/google-sign-in/src/GoogleSignInModule.ts
+++ apps/mobile/modules/google-sign-in/src/GoogleSignInModule.ts
@@ -1,12 +1,14 @@
-import { NativeModule, requireNativeModule } from 'expo';
+import { NativeModule, requireNativeModule } from 'expo'
 
-import { GoogleSignInModuleEvents } from './GoogleSignIn.types';
+type ConfigureOptions = {
+  webClientId: string
+}
 
-declare class GoogleSignInModule extends NativeModule<GoogleSignInModuleEvents> {
-  PI: number;
-  hello(): string;
-  setValueAsync(value: string): Promise<void>;
+declare class GoogleSignInModule extends NativeModule {
+  configure(options: ConfigureOptions): void
+
+  signIn(): Promise<string>
 }
 
 // This call loads the native module object from the JSI.
-export default requireNativeModule<GoogleSignInModule>('GoogleSignIn');
+export default requireNativeModule<GoogleSignInModule>('GoogleSignIn')
