41e5290d8d6ecc3c892aa3b8fbe4d73148bfe642
Leung Cheng <leung-cheng@outlook.com>
2025-11-04 19:03:19 +0800

    Remove codes

--- apps/mobile/modules/google-sign-in/index.ts
+++ apps/mobile/modules/google-sign-in/index.ts
@@ -1,5 +1,3 @@
 // Reexport the native module. On web, it will be resolved to GoogleSignInModule.web.ts
 // and on native platforms to GoogleSignInModule.ts
-export { default } from './src/GoogleSignInModule';
-export { default as GoogleSignInView } from './src/GoogleSignInView';
-export * from  './src/GoogleSignIn.types';
+export { default } from './src/GoogleSignInModule'
