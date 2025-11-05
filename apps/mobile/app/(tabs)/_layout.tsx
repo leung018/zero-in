039fde8923a240c774b9e162ca0cd68b3fd25cff
Leung Cheng <leung-cheng@outlook.com>
2025-11-05 19:13:43 +0800

    Example fetch and listen to data

--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -31,6 +31,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />
         }}
       />
+      <Tabs.Screen
+        name="fire"
+        options={{
+          title: 'Firestore',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="gearshape.fill" color={color} />
+        }}
+      />
     </Tabs>
   )
 }
---
+++ apps/mobile/app/(tabs)/fire.tsx
@@ -0,0 +1,65 @@
+import { FirebaseAuthTypes, getAuth, onAuthStateChanged } from '@react-native-firebase/auth'
+import { doc, getDoc, getFirestore, onSnapshot } from '@react-native-firebase/firestore'
+import { useEffect, useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function FirestoreExperimentScreen() {
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null)
+  const [endAt, setEndAt] = useState<string>('Loading...')
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(getAuth(), (firebaseUser) => {
+      setUser(firebaseUser)
+    })
+    return unsubscribe
+  }, [])
+
+  useEffect(() => {
+    if (user) {
+      getTimeDataFromFirestore(user.uid).then((endAt) => {
+        setEndAt(endAt)
+      })
+    }
+  }, [user])
+
+  useEffect(() => {
+    if (user) {
+      const unsubscribe = onSnapshot(
+        doc(getFirestore(), 'users', user.uid, 'application', 'timerState'),
+        (doc) => {
+          setEndAt(doc.data()?.endAt ?? 'No Data')
+        }
+      )
+      return unsubscribe
+    }
+  }, [user])
+
+  if (!user) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading User</Text>
+      </View>
+    )
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>{new Date(endAt).toLocaleString()}</Text>
+    </View>
+  )
+}
+
+async function getTimeDataFromFirestore(userId: string): Promise<string> {
+  const result = await getDoc(doc(getFirestore(), 'users', userId, 'application', 'timerState'))
+  return result.data()!.endAt ?? 'No Data'
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f0f0',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20
+  }
+})
