621c3deaf1a3a057917425fa45500b9c63a4573b
Leung Cheng <leung-cheng@outlook.com>
2025-11-05 13:29:51 +0800

    Experiment signIn in swift

--- apps/mobile/modules/google-sign-in/ios/GoogleSignInModule.swift
+++ apps/mobile/modules/google-sign-in/ios/GoogleSignInModule.swift
@@ -2,22 +2,55 @@ import ExpoModulesCore
 import GoogleSignIn
 
 public class GoogleSignInModule: Module {
-  private var webClientId: String?
-  
-  public func definition() -> ModuleDefinition {
-    Name("MyGoogleSignIn")
-    
-    Function("configure") { (options: ConfigureOptions) in
-      
-    }
-    
-    AsyncFunction("signIn") { (promise: Promise) in
-      promise.resolve("")
-    }
-  }
-}
+    private var webClientId: String?
 
-struct ConfigureOptions: Record {
-  @Field
-  var webClientId: String
+    public func definition() -> ModuleDefinition {
+        Name("MyGoogleSignIn")
+
+        Function("configure") {}
+
+        AsyncFunction("signIn") { (promise: Promise) in
+            guard
+                let windowScene = UIApplication.shared.connectedScenes.first
+                    as? UIWindowScene,
+                let rootViewController = windowScene.windows.first?
+                    .rootViewController
+            else {
+                promise.reject(
+                    Exception(
+                        name: "NoViewController",
+                        description: "Cannot find root view controller"
+                    )
+                )
+                return
+            }
+
+            GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController)
+            { signInResult, error in
+                if let error = error {
+                    promise.reject(
+                        Exception(
+                            name: "SignInError",
+                            description: error.localizedDescription
+                        )
+                    )
+                    return
+                }
+
+                guard let result = signInResult,
+                    let idToken = result.user.idToken?.tokenString
+                else {
+                    promise.reject(
+                        Exception(
+                            name: "SignInError",
+                            description: "Failed to get ID token"
+                        )
+                    )
+                    return
+                }
+
+                promise.resolve(idToken)
+            }
+        }
+    }
 }
