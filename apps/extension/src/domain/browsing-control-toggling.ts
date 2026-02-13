81f2bb6f0279ea8984865360026ea16dbddf7541
Leung Cheng <leung-cheng@outlook.com>
2026-02-13 21:18:30 +0800

    Move shouldActivateBlocking logic into shared

--- apps/extension/src/domain/browsing-control-toggling.ts
+++ apps/extension/src/domain/browsing-control-toggling.ts
@@ -1,30 +1,19 @@
-import type { WeeklySchedule } from '@zero-in/shared/domain/schedules'
 import { WeeklySchedulesStorageService } from '@zero-in/shared/domain/schedules/storage'
+import {
+  ShouldActivateBlockingTeller,
+  TimerInfoGetter
+} from '@zero-in/shared/domain/should-activate-blocking'
 import { TimerBasedBlockingRulesStorageService } from '@zero-in/shared/domain/timer-based-blocking/storage'
 import { FocusSessionRecordsStorageService } from '@zero-in/shared/domain/timer/record/storage'
-import { isSameDay } from '@zero-in/shared/utils/date'
 import { FakeBrowsingControlService, type BrowsingControlService } from '../infra/browsing-control'
 import { BrowsingRulesStorageService } from './browsing-rules/storage'
 import { Duration } from './timer/duration'
 import { TimerStage } from './timer/stage'
 
-interface TimerInfoGetter {
-  getTimerInfo(): {
-    timerStage: TimerStage
-    isRunning: boolean
-    remaining: Duration
-    longBreak: Duration
-    shortBreak: Duration
-  }
-}
-
 export class BrowsingControlTogglingService {
   private browsingControlService: BrowsingControlService
   private browsingRulesStorageService: BrowsingRulesStorageService
-  private weeklySchedulesStorageService: WeeklySchedulesStorageService
-  private focusSessionRecordsStorageService: FocusSessionRecordsStorageService
-  private timerBasedBlockingRulesStorageService: TimerBasedBlockingRulesStorageService
-  private timerInfoGetter: TimerInfoGetter
+  private shouldActivateBlockingTeller: ShouldActivateBlockingTeller
 
   static createFake({
     browsingControlService = new FakeBrowsingControlService(),
@@ -69,90 +58,19 @@ export class BrowsingControlTogglingService {
   }) {
     this.browsingControlService = browsingControlService
     this.browsingRulesStorageService = browsingRulesStorageService
-    this.weeklySchedulesStorageService = weeklySchedulesStorageService
-    this.timerBasedBlockingRulesStorageService = timerBasedBlockingRulesStorageService
-    this.focusSessionRecordsStorageService = focusSessionRecordsStorageService
-    this.timerInfoGetter = timerInfoGetter
+    this.shouldActivateBlockingTeller = new ShouldActivateBlockingTeller({
+      weeklySchedulesStorageService,
+      focusSessionRecordsStorageService,
+      timerBasedBlockingRulesStorageService,
+      timerInfoGetter
+    })
   }
 
   async run(): Promise<void> {
-    if (await this.shouldActivateBrowsingRules()) {
+    if (await this.shouldActivateBlockingTeller.shouldActivateBlocking()) {
       const browsingRules = await this.browsingRulesStorageService.get()
       return this.browsingControlService.setAndActivateNewRules(browsingRules)
     }
     return this.browsingControlService.deactivateExistingRules()
-  }
-
-  private async shouldActivateBrowsingRules(): Promise<boolean> {
-    const timerBasedBlockingRules = await this.timerBasedBlockingRulesStorageService.get()
-
-    if (timerBasedBlockingRules.pauseBlockingWhenTimerNotRunning && !this.isRunning()) {
-      return false
-    }
-
-    if (timerBasedBlockingRules.pauseBlockingDuringBreaks && this.isInBreak()) {
-      return false
-    }
-
-    const schedules = await this.weeklySchedulesStorageService.get()
-
-    if (schedules.length === 0) {
-      return true
-    }
-
-    return this.isAnyScheduleActive(schedules)
-  }
-
-  private async isAnyScheduleActive(
-    inputSchedules: ReadonlyArray<WeeklySchedule>
-  ): Promise<boolean> {
-    const now = new Date()
-    const schedules = inputSchedules.filter((schedule) => schedule.isContain(now))
-
-    const focusSessionRecords = await this.focusSessionRecordsStorageService.get()
-
-    for (const schedule of schedules) {
-      if (!schedule.targetFocusSessions) {
-        return true
-      }
-      const completedSessions = focusSessionRecords.filter(
-        (record) => isSameDay(record.completedAt, now) && schedule.isContain(record.completedAt)
-      )
-      if (completedSessions.length < schedule.targetFocusSessions) {
-        return true
-      }
-    }
-
-    return false
-  }
-
-  private isInBreak(): boolean {
-    const timerInfo = this.timerInfoGetter.getTimerInfo()
-    if (timerInfo.timerStage === TimerStage.FOCUS) {
-      return false
-    }
-    if (timerInfo.isRunning) {
-      return true
-    }
-
-    // If user hasn't pressed the start of the timer even the stage is break, still is not in break yet.
-    if (
-      timerInfo.timerStage === TimerStage.SHORT_BREAK &&
-      timerInfo.shortBreak.totalMilliseconds <= timerInfo.remaining.totalMilliseconds
-    ) {
-      return false
-    }
-    if (
-      timerInfo.timerStage === TimerStage.LONG_BREAK &&
-      timerInfo.longBreak.totalMilliseconds <= timerInfo.remaining.totalMilliseconds
-    ) {
-      return false
-    }
-    return true
-  }
-
-  private isRunning(): boolean {
-    const timerInfo = this.timerInfoGetter.getTimerInfo()
-    return timerInfo.isRunning
   }
 }
