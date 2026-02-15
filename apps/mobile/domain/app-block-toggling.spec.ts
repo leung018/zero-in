4a9560318f05f7e6ee6f489b90bc0b0376b0aedb
Leung Cheng <leung-cheng@outlook.com>
2026-02-16 00:26:19 +0800

    Draft test case of target focus session

--- apps/mobile/domain/app-block-toggling.spec.ts
+++ apps/mobile/domain/app-block-toggling.spec.ts
@@ -3,6 +3,8 @@ import { WeeklySchedule } from '@zero-in/shared/domain/schedules/index'
 import { WeeklySchedulesStorageService } from '@zero-in/shared/domain/schedules/storage'
 import { Weekday } from '@zero-in/shared/domain/schedules/weekday'
 import { Time } from '@zero-in/shared/domain/time'
+import { newFocusSessionRecord } from '../../../packages/shared/src/domain/timer/record'
+import { FocusSessionRecordsStorageService } from '../../../packages/shared/src/domain/timer/record/storage'
 import { AppBlockTogglingService } from './app-block-toggling'
 
 describe('AppBlockTogglingService', () => {
@@ -39,6 +41,48 @@ describe('AppBlockTogglingService', () => {
     expect(appBlocker.getBlockingScheduleSpan()).toBeNull()
   })
 
+  it.skip('should only consider schedules that have not completed target focus sessions', async () => {
+    jest.setSystemTime(new Date('2026-01-05T12:00:00')) // 2026-01-05 is Monday
+    const { appBlocker, focusSessionRecordsStorageService, togglingService } =
+      await runAppBlockToggling({
+        weeklySchedules: [
+          new WeeklySchedule({
+            weekdaySet: new Set([Weekday.MON, Weekday.TUE]),
+            startTime: new Time(9, 0),
+            endTime: new Time(17, 0),
+            targetFocusSessions: 2
+          })
+        ],
+        focusSessionRecords: [
+          newFocusSessionRecord({
+            completedAt: new Date('2026-01-04T10:00:00')
+          }),
+          newFocusSessionRecord({
+            completedAt: new Date('2026-01-05T10:00:00')
+          })
+        ]
+      })
+
+    expect(appBlocker.getBlockingScheduleSpan()).toEqual({
+      start: new Date('2026-01-05T09:00:00'),
+      end: new Date('2026-01-05T17:00:00')
+    })
+
+    // After adding another completed focus session within Monday schedule, it should consider the schedule as completed
+    await focusSessionRecordsStorageService.save([
+      newFocusSessionRecord({
+        completedAt: new Date('2026-01-05T11:00:00')
+      })
+    ])
+
+    await togglingService.run()
+
+    expect(appBlocker.getBlockingScheduleSpan()).toEqual({
+      start: new Date('2026-01-06T09:00:00'),
+      end: new Date('2026-01-06T17:00:00')
+    })
+  })
+
   it('should always block app only if no schedule is set', async () => {
     const { appBlocker, weeklySchedulesStorageService, togglingService } =
       await runAppBlockToggling({
@@ -72,19 +116,29 @@ async function runAppBlockToggling({
       startTime: new Time(9, 0),
       endTime: new Time(17, 0)
     })
-  ]
+  ],
+  focusSessionRecords = [newFocusSessionRecord()]
 } = {}) {
   const weeklySchedulesStorageService = WeeklySchedulesStorageService.createFake()
+  const focusSessionRecordsStorageService = FocusSessionRecordsStorageService.createFake()
   const appBlocker = new FakeAppBlocker()
   const togglingService = AppBlockTogglingService.createFake({
     weeklySchedulesStorageService,
+    focusSessionRecordsStorageService,
     appBlocker
   })
 
   await weeklySchedulesStorageService.save(weeklySchedules)
+  await focusSessionRecordsStorageService.save(focusSessionRecords)
+
   const blockingScheduleSpan = await togglingService.run()
 
   expect(blockingScheduleSpan).toEqual(appBlocker.getBlockingScheduleSpan())
 
-  return { weeklySchedulesStorageService, appBlocker, togglingService }
+  return {
+    weeklySchedulesStorageService,
+    appBlocker,
+    togglingService,
+    focusSessionRecordsStorageService
+  }
 }
