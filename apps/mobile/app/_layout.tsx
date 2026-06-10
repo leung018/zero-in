732a5d81596c5a6dede2c2416f3f1feded447590
Leung Cheng <leung-cheng@outlook.com>
2026-06-10 20:52:27 +0800

    Fix reopen the app keep trigger tapped handler

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout() {
   useEffect(() => {
     if (lastNotificationResponse) {
       onScheduleEndNotificationTapped(lastNotificationResponse)
+      Notifications.clearLastNotificationResponse()
     }
   }, [lastNotificationResponse])
 
