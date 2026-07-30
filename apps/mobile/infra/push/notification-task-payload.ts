16d1fae03bd44402ea58e3e079e0c03b3e942110
Leung Cheng <leung-cheng@outlook.com>
2026-07-30 21:15:40 +0800

    Update comment

--- apps/mobile/infra/push/notification-task-payload.ts
+++ apps/mobile/infra/push/notification-task-payload.ts
@@ -4,8 +4,11 @@
 // - iOS: data.dataString
 
 // TODO: Haven't test ios Part. Can also consider removing unit tests of that because may not be so useful.
+// Update below comment after ios part is tested.
 export function extractNotificationTaskPayload(data: any): any {
+  // Android: data.data.dataString
   const dataString = data?.notification?.request?.content?.dataString ?? data?.data?.dataString
+
   if (!dataString) return undefined
   try {
     return JSON.parse(dataString)
