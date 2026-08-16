bdb0104d0859f915bc90b5d438a99b8565f1650e
Leung Cheng <leung-cheng@outlook.com>
2026-08-16 17:33:16 +0800

    Move permission to separate file

--- apps/mobile/modules/app-blocker/index.ts
+++ apps/mobile/modules/app-blocker/index.ts
@@ -1,4 +1,5 @@
 // Reexport the native module. On web, it will be resolved to AppBlockerModule.web.ts
 // and on native platforms to AppBlockerModule.ts
-export { PermissionStatus, PermissionType, appBlocker } from './src/AppBlockerModule'
+export { appBlocker } from './src/AppBlockerModule'
 export { default as AppPickerView } from './src/AppPickerView'
+export { PermissionStatus, PermissionType } from './src/permission'
